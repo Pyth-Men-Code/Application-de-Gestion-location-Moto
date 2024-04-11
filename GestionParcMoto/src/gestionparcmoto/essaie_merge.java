46c02c923dfdced4b40d8f8c43a0d415c898ca03
Soufiane Belhabibe <soufiane.linedata@se.linedata.com>
2024-04-11 16:06:06 +0200

    error manager added

---
+++ GestionParcMoto/src/gestionparcmoto/essaie_merge.java
@@ -0,0 +1,12 @@
+package gestionparcmoto;
+
+public class essaie_merge extends InterfacePrincipale {
+
+    private static final String name = "hello";
+
+public static manage_erreurr(){
+if (name.isEmpty()){
+   System.out.println("renseigner nom");
+}
+}
+}
